6e112c85de17381ed28ef61a7803370107da3f03
Lior <lior@L01015153.local>
2020-05-22 11:44:26 +0200

    fix

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	res := hello()
-	expected := "Hello Jenkins2"
+	expected := "Hello Jenkins"
 	if res != expected {
 		t.Errorf("Hello was incorrect, got: %s, want: %s.", res, expected)
 	}
